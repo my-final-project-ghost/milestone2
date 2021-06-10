85e91807e297dfd21b2ab4e77ff86d73e01cb760
MahmoudKhaled20184434 <85295806+MahmoudKhaled20184434@users.noreply.github.com>
2021-06-10 03:32:46 +0200

    work3

---
+++ Project2/Project/JS/JavaScript.js
@@ -0,0 +1,13 @@
+﻿ 
+    if (val.isvalid) {
+        document.getElementById('txtpassword').style.border = '1px solid #333';
+    }
+    else {
+        document.getElementById('txtpassword').style.border = '1px solid red';
+    }
+}
+
+//if (document.getElementById('RegularExpressionValidator1').textContent == "ss") {
+//    document.getElementById('txtfname').style.borderColor = "red"
+
+//}
